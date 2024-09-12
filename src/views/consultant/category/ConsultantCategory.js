4ed2621bd1f8e8079d80f8beb49af764a81b4ee1
Nguyen Hoang Quoc <quocnh@chinhnhanvn.com>
2024-09-12 09:37:38 +0700

    Merge sang-dev

--- src/views/consultant/category/ConsultantCategory.js
+++ src/views/consultant/category/ConsultantCategory.js
@@ -233,25 +233,25 @@ function ConsultantCategory() {
 
   const handleDeleteAll = async () => {
     console.log('>>> check undeal', selectedCheckbox)
-    // alert('Chức năng đang thực hiện...')
-    try {
-      const response = await axiosClient.post(`admin/delete `, {
-        data: selectedCheckbox,
-      })
 
-      if (response.data.status === true) {
-        toast.success('Xóa tất cả danh mục thành công!')
-        fetchDataConsultantCate()
-        setSelectedCheckbox([])
-      }
+    // try {
+    //   const response = await axiosClient.post(`admin/delete `, {
+    //     data: selectedCheckbox,
+    //   })
 
-      if (response.data.status === false && response.data.mess == 'no permission') {
-        toast.warn('Bạn không có quyền thực hiện tác vụ này!')
-      }
-    } catch (error) {
-      console.error('xóa lỗi error', error)
-      toast.error('Đã xảy ra lỗi. Vui lòng thử lại!')
-    }
+    //   if (response.data.status === true) {
+    //     toast.success('Xóa tất cả danh mục thành công!')
+    //     fetchDataConsultantCate()
+    //     setSelectedCheckbox([])
+    //   }
+
+    //   if (response.data.status === false && response.data.mess == 'no permission') {
+    //     toast.warn('Bạn không có quyền thực hiện tác vụ này!')
+    //   }
+    // } catch (error) {
+    //   console.error('xóa lỗi error', error)
+    //   toast.error('Đã xảy ra lỗi. Vui lòng thử lại!')
+    // }
   }
 
   const items =
