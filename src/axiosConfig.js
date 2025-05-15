5a1351759ce507a6b9a0bf49b6ac2202ade5808e
lexuanlocnk <135822299+lexuanlocnk@users.noreply.github.com>
2025-05-15 11:34:17 +0700

    modify axiosconfig

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: 'https://ap.chinhnhan.vn/api/',
+  baseURL: 'https://ap.chinhnhan.net/api/',
   headers: {
     'Content-Type': 'application/json',
     Authorization: localStorage.getItem('adminCN')
@@ -24,7 +24,7 @@ axiosClient.interceptors.request.use(
 )
 
 // Configuration for images
-const imageBaseUrl = 'https://ap.chinhnhan.vn/uploads/'
-const mainUrl = 'https://test.chinhnhan.vn/'
+const imageBaseUrl = 'https://ap.chinhnhan.net/uploads/'
+const mainUrl = 'https:chinhnhan.net/'
 
 export { axiosClient, imageBaseUrl, mainUrl }
